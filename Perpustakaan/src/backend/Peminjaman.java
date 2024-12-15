69478b7891cc1cca6afa7e61fb0f5756e68dfde8
Rio Tri Prayogo <riotriprayogo31@gmail.com>
2024-12-15 15:01:27 +0700

    feat: push all fixed Perpustakaan

---
+++ Perpustakaan/src/backend/Peminjaman.java
@@ -0,0 +1,136 @@
+package backend;
+
+import java.sql.*;
+import java.util.ArrayList;
+
+public class Peminjaman {
+    private int idPeminjaman;   
+    private int idPegawai;     
+    private Anggota anggota;
+    private Buku buku;
+    private String tanggalPinjam;
+    private String tanggalKembali;
+
+    public Peminjaman() {
+    }
+
+    public Peminjaman(int idPeminjaman, int idPegawai, Anggota anggota, Buku buku, String tanggalPinjam, String tanggalKembali) {
+        this.idPeminjaman = idPeminjaman;
+        this.idPegawai = idPegawai;
+        this.anggota = anggota;
+        this.buku = buku;
+        this.tanggalPinjam = tanggalPinjam;
+        this.tanggalKembali = tanggalKembali;
+    }
+
+    public int getIdPeminjaman() {
+        return idPeminjaman;
+    }
+
+    public void setIdPeminjaman(int idPeminjaman) {
+        this.idPeminjaman = idPeminjaman;
+    }
+
+    public int getIdPegawai() {
+        return idPegawai;
+    }
+
+    public void setIdPegawai(int idPegawai) {
+        this.idPegawai = idPegawai;
+    }
+
+    public Anggota getAnggota() {
+        return anggota;
+    }
+
+    public void setAnggota(Anggota anggota) {
+        this.anggota = anggota;
+    }
+
+    public Buku getBuku() {
+        return buku;
+    }
+
+    public void setBuku(Buku buku) {
+        this.buku = buku;
+    }
+
+    public String getTanggalPinjam() {
+        return tanggalPinjam;
+    }
+
+    public void setTanggalPinjam(String tanggalPinjam) {
+        this.tanggalPinjam = tanggalPinjam;
+    }
+
+    public String getTanggalKembali() {
+        return tanggalKembali;
+    }
+
+    public void setTanggalKembali(String tanggalKembali) {
+        this.tanggalKembali = tanggalKembali;
+    }
+
+    public void save() {
+        if (idPeminjaman == 0) {
+            String query = "INSERT INTO peminjaman (idpegawai, idanggota, idbuku, tanggalpinjam, tanggalKembali) VALUES ("
+                    + idPegawai + ", "
+                    + anggota.getIdAnggota() + ", "
+                    + buku.getIdbuku() + ", '"
+                    + tanggalPinjam + "', NULL)";
+            this.idPeminjaman = DBHelper.insertQueryGetId(query);
+        } else {
+            String query = "UPDATE peminjaman SET tanggalKembali = '" + tanggalKembali
+                    + "' WHERE idpeminjaman = " + idPeminjaman;
+            DBHelper.executeQuery(query);
+        }
+    }
+
+    public Peminjaman getById(int id) {
+        Peminjaman peminjaman = null;
+        ResultSet rs = DBHelper.selectQuery("SELECT * FROM peminjaman WHERE idpeminjaman = " + id);
+
+        try {
+            if (rs.next()) {
+                peminjaman = new Peminjaman(
+                        rs.getInt("idpeminjaman"),
+                        rs.getInt("idpegawai"),
+                        new Anggota().getById(rs.getInt("idanggota")),
+                        new Buku().getById(rs.getInt("idbuku")),
+                        rs.getString("tanggalpinjam"),
+                        rs.getString("tanggalKembali")
+                );
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return peminjaman;
+    }
+
+    public ArrayList<Peminjaman> getAll() {
+        ArrayList<Peminjaman> listPeminjaman = new ArrayList<>();
+        ResultSet rs = DBHelper.selectQuery("SELECT * FROM peminjaman");
+
+        try {
+            while (rs.next()) {
+                Peminjaman peminjaman = new Peminjaman(
+                        rs.getInt("idpeminjaman"),
+                        rs.getInt("idpegawai"),
+                        new Anggota().getById(rs.getInt("idanggota")),
+                        new Buku().getById(rs.getInt("idbuku")),
+                        rs.getString("tanggalpinjam"),
+                        rs.getString("tanggalKembali")
+                );
+                listPeminjaman.add(peminjaman);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return listPeminjaman;
+    }
+
+    public void delete() {
+        String query = "DELETE FROM peminjaman WHERE idpeminjaman = " + this.idPeminjaman;
+        DBHelper.executeQuery(query);
+    }
+}
